e7abcb2a16f9975653dbd608e1753ac3418b02cb
Ishan13 <shrestha.ishan2@gmail.com>
2024-09-13 15:57:11 -0400

    Day1 band name generator

---
+++ Day1.py
@@ -0,0 +1,38 @@
+# print("""
+# Day 1- Python Print Function
+# The function is declared like this:
+# print('what to print')
+# """)
+
+# print("Day 1- Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("Day 1- Python Print Function \nThe function is declared like this: \nprint('What to print')")
+
+# print("Hello" + " " + "Angela")
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "World")')
+# print("New lines can be created with a backslash and n.")
+
+# print("Hello " + input("What is your name?")+ "!")
+
+# print(len(input("What is your name?")))
+
+# my_string = input("What is your name?")
+# length = len(my_string)
+# print(length)
+
+# a = input("a:")
+# b = input("b:")
+# a, b = b, a
+# or c=a a=b b=c
+# print("a = " + a)
+# print("b = " + b)
+
+print("Welcome to the Band Name Generator!")
+city = input("What's name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+print("Your band name could be " + city + " " + pet)
